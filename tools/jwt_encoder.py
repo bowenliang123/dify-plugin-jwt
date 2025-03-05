38253285d404d0842e91b4140bee6e42edcf50e3
Bowen Liang <liangbowen@gf.com.cn>
2025-03-05 21:20:42 +0800

    update typing

--- tools/jwt_encoder.py
+++ tools/jwt_encoder.py
@@ -1,5 +1,5 @@
 from collections.abc import Generator
-from typing import Any
+from typing import Any, Optional, Mapping
 
 import jwt
 from dify_plugin import Tool
@@ -11,16 +11,18 @@ from tools.utils import jwt_utils
 class JwtEncoderTool(Tool):
     def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
         param_payload: str = tool_parameters.get("payload", "{}") or "{}"
-        param_headers: str = tool_parameters.get("headers", "{}") or "{}"
+        param_headers: str = tool_parameters.get("headers")
         key: str = tool_parameters.get("key")
         algorithm: str = tool_parameters.get("algorithm")
 
         # validation
         jwt_utils.check_valid_algorithm(algorithm)
-        # jwt_utils.check_key_length(key, algorithm)
+        if not key:
+            raise ValueError("Invalid input for decryption key")
 
-        payload: dict[str, Any] = jwt_utils.parse_valid_json(param_payload, "payload")
-        headers: dict[str, Any] = jwt_utils.parse_valid_json(param_headers, "headers")
-        result = jwt.encode(payload=payload, key=key, algorithm=algorithm, headers=headers)
+        payload: Mapping[str, Any] = jwt_utils.parse_valid_json(param_payload, "payload")
+        headers: Optional[Mapping[str, Any]] = jwt_utils.parse_valid_json(param_headers, "headers") \
+            if param_headers else None
+        result: str = jwt.encode(payload=payload, key=key, algorithm=algorithm, headers=headers)
 
         yield self.create_text_message(result)
